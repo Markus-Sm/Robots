6468c5fdc2b74e9db7587ab530df770632a7c26e
Marek Samofal <marek.samofal@gmail.com>
2023-12-14 12:17:55 +0100

    docs: Optimizations

--- src/App.js
+++ src/App.js
@@ -28,10 +28,21 @@ class App extends Component {
 			)
 	}
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
 	render() {
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
 		
-		const filteredMonsters = this.state.monsters.filter(monster => {
-			return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+		const filteredMonsters = monsters.filter(monster => {
+			return monster.name.toLocaleLowerCase().includes(searchField)
 		})
 
 		return (
@@ -40,13 +51,7 @@ class App extends Component {
 					className='search-box'
 					type='search'
 					placeholder='search monsters'
-					onChange={event => {
-						const searchField = event.target.value.toLocaleLowerCase()
-
-						this.setState(() => {
-							return { searchField }
-						})
-					}}
+					onChange={ onSearchChange }
 				/>
 				{filteredMonsters.map(monster => {
 					return (
