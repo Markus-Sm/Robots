a394bf9d15a7159544eb836e27122121aa508f0b
Marek Samofal <marek.samofal@gmail.com>
2023-12-14 11:06:33 +0100

    Storing original data, part 2

--- src/App.js
+++ src/App.js
@@ -1,61 +1,65 @@
-import { Component } from 'react';
+import { Component } from 'react'
 
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  constructor(){
-    super();
+	constructor() {
+		super()
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-  }
+		this.state = {
+			monsters: [],
+			searchField: '',
+		}
+	}
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => this.setState(() => {
-      return {monsters: users}
-    },
-    () => {
-      console.log(this.state);
-    }
-    ));
-  }
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users =>
+				this.setState(
+					() => {
+						return { monsters: users }
+					},
+					() => {
+						console.log(this.state)
+					}
+				)
+			)
+	}
 
 	render() {
+		console.log('render')
+		const filteredMonsters = this.state.monsters.filter(monster => {
+			return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+		})
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-    });
+		return (
+			<div className='App'>
+				<input
+					className='search-box'
+					type='search'
+					placeholder='search monsters'
+					onChange={event => {
+						const searchField = event.target.value.toLocaleLowerCase()
 
-		return(
-      <div className='App'>
-        <input 
-        className='search-box' 
-        type='search' 
-        placeholder='search monsters' 
-        onChange={(event) => {
-          const searchField = event.target.value.toLocaleLowerCase();
-
-          this.setState(
-            () => {
-            return { searchField };
-          }
-        );
-        }}/>
-       {filteredMonsters.map((monster) => {
-        return(
-          <div key={monster.id}>
-          <h1>{monster.name}, {monster.username}</h1>
-          <p>{monster.website}</p>
-        </div>
-        );
-      })}
-    </div>
-    );
+						this.setState(() => {
+							return { searchField }
+						})
+					}}
+				/>
+				{filteredMonsters.map(monster => {
+					return (
+						<div key={monster.id}>
+							<h1>
+								{monster.name}, {monster.username}
+							</h1>
+							<p>{monster.website}</p>
+						</div>
+					)
+				})}
+			</div>
+		)
 	}
 }
 
